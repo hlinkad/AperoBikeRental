cee354737ac782bc1747f11d66cd38d10d1f488c
st4mpik <leonard.merva@gmail.com>
2018-11-21 15:06:20 +0100

    angular excepitons handling

--- AperoRental-SPA/src/app/nav/nav.component.ts
+++ AperoRental-SPA/src/app/nav/nav.component.ts
@@ -1,33 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../_services/auth.service';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../_services/auth.service";
 
 @Component({
-  selector: 'app-nav',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  selector: "app-nav",
+  templateUrl: "./nav.component.html",
+  styleUrls: ["./nav.component.css"]
 })
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
   login() {
-    this.authService.login(this.model).subscribe(next => {
-      console.log('Logged in sucessfully');
-    }, error => {
-      console.log('Failed to login');
-    });
+    this.authService.login(this.model).subscribe(
+      next => {
+        console.log("Logged in sucessfully");
+      },
+      error => {
+        console.log(error);
+      }
+    );
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     return !!token;
   }
 
   logout() {
-    localStorage.removeItem('token');
-    console.log('logged out');
+    localStorage.removeItem("token");
+    console.log("logged out");
   }
 }
